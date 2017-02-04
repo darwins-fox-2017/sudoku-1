66039784296226b228770057d19491c7dd909112
mfpebrizal <ftm.pebrizal@gmail.com>
2017-02-04 20:39:48 +0700

    sedikit Refaktor

--- sudoku.js
+++ sudoku.js
@@ -47,39 +47,28 @@ class Sudoku{
   solve(){
     let x = 0
     let y = 0
-    let temp = this.board
     let flag = true
-    let tempXY = [] // tempat merekam indeks yang dikunjungi beserta nilai yang diassign
-    console.log(temp) // papan awal
+    let tempXY = []
     let num = 1
 
     while(x<9 || y<9){
       if(this.board[x][y] === 0){
 
         if(this.checkBox(x,y,num) && this.checkRow(x,num) && this.checkColumn(y,num) && flag){
-          console.log(this.board) // proses pengisian
           flag =true
           tempXY.push({'x' : x, 'y': y, 'nilai': num})
-          this.board[x][y] = num // assign jadi num (kondisi nya semua terpenuhi)
-          num = 1 // pindah ke kotak selanjutnya dgn reset num jadi 1
-          y++
-          if(y == 9){
-            y = 0
-            x++
-            if(x == 9){
-              return this.board
-            }
-          }
+          this.board[x][y] = num
+          num = 1
         }else{
           if(num<9){
-            num++// kalau angka g sesuai di kotak, tambah dan ulangi ke atas
+            num++
           }
-          else{//artinya semua angka g cocok , balik ke koordinat yg diisi sebelumnya
+          else{
             x = tempXY[tempXY.length-1].x
             y = tempXY[tempXY.length-1].y
             num = tempXY[tempXY.length-1].nilai + 1
             if(num > 9){
-              flag = false // angka kotak sebelumnya juga 9, balik ke kotak sebelumnya
+              flag = false
             }else{
               flag = true
             }
@@ -105,7 +94,7 @@ class Sudoku{
 }
 
 var fs = require('fs')
-var board_string = fs.readFileSync('set-01_sample.unsolved.txt')//('set-01_sample.unsolved.txt')
+var board_string = fs.readFileSync('set-01_sample.unsolved.txt')
   .toString()
   .split("\n")[13]
 
@@ -114,4 +103,4 @@ var game = new Sudoku(board_string)
 // Remember: this will just fill out what it can and not "guess"
 console.log(game.solve())
 
-// console.log(game.board())
+console.log(game.boardMaker())
