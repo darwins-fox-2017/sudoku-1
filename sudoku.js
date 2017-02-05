fa92f4af8662d9f12a87edd74f4840946663f8ea
Diky Arga <dikyarga.id@gmail.com>
2017-02-05 13:20:57 +0700

    Generate Sudoku Board

--- sudoku.js
+++ sudoku.js
@@ -1,7 +1,24 @@
 "use strict"
 
 class Sudoku {
-  constructor(board_string) {}
+  constructor(board_string) {
+    this.initial_value = board_string
+    this.board = this.generateBoard()
+  }
+
+  generateBoard(){
+    let parse = this.initial_value.split('')
+    let board = []
+    for (let i = 0; i < 9; i++) {
+      board[i] = []
+      for (let j = 0; j < 9; j++) {
+        board[i].push(Number(parse[0]))
+        // parse.shift()
+      }
+    }
+    console.log(board);
+    return board
+  }
 
   solve() {}
 
@@ -21,4 +38,4 @@ var game = new Sudoku(board_string)
 // Remember: this will just fill out what it can and not "guess"
 game.solve()
 
-console.log(game.board())
+// console.log(game.board())
